070d19efb0fb716d62095f196a2bfea0b8129806
Neiso <djulian.ouatla@outlook.com>
2020-04-10 12:54:20 +0200

    Clean up the code

--- sylvie/expo/views.py
+++ sylvie/expo/views.py
@@ -5,6 +5,4 @@ from .models import ExpoModel
 
 def renderExpo(request):
     expos = ExpoModel.objects.all()
-    for expo in expos:
-        print (expo.link)
     return(render(request, 'expo.html', {'expos':expos}))
